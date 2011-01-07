edd17c21cea34961224a6815949bcd05c9be947e
arpit1712@gmail.com <arpit1712@gmail.com@0cb6586d-dc45-c2ee-bce3-b71acda30c77>
2011-01-07 01:45:36 +0000

    Initial import.

---
+++ src/src/dbms.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : dbms.cpp
+// Author      : Arpit
+// Version     :
+// Copyright   : Feel free to distribute
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
