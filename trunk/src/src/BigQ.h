39d36c0243312e376aa8a28306a6c0cea93cf424
arpit1712@gmail.com <arpit1712@gmail.com@0cb6586d-dc45-c2ee-bce3-b71acda30c77>
2011-02-09 02:59:11 +0000

    implemented getRunsFromInputPipe() for BigQ pthread_create is still giving errors

--- trunk/src/src/BigQ.h
+++ trunk/src/src/BigQ.h
@@ -2,18 +2,30 @@
 #define BIGQ_H
 #include <pthread.h>
 #include <iostream>
+#include <vector>
 #include "Pipe.h"
 #include "File.h"
 #include "Record.h"
+#include "Defs.h"
+#include "DBFile.h"
 
 using namespace std;
 
 class BigQ {
-
+	DBFile m_runFile;
+	Pipe *m_pInPipe, *m_pOutPipe;
+	OrderMaker *m_pSortOrder;
+	int m_nRunLen, m_nPageCount;
+	void quickSortRun(Record**, int, int, ComparisonEngine&);
+	int partition(Record**, int, int, ComparisonEngine&);
+	void swap(Record*&, Record*&);
+	void appendRunToFile(vector<Record*>&);
+	void quickSortRun(vector<Record*>& aRun, int begin, int end, ComparisonEngine &ce);
+	int partition(vector<Record*>&, int, int, ComparisonEngine&);
 public:
-
 	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
 	~BigQ ();
+	friend void* getRunsFromInputPipe(void*);
 };
 
 #endif
