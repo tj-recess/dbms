b21aec801c7a545400bbc8b3c098683530651801
malvika85@gmail.com <malvika85@gmail.com@0cb6586d-dc45-c2ee-bce3-b71acda30c77>
2011-04-03 23:26:19 +0000

    Statistics class structure

--- src/src/Statistics.h
+++ src/src/Statistics.h
@@ -1,10 +1,32 @@
 #ifndef STATISTICS_
 #define STATISTICS_
 #include "ParseTree.h"
+#include <map>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Structure to store stats of a relation
+struct AttsStats
+{
+	int numTuples;		// total num of rows in this relation
+	int numPartition;	// partition this relation belongs to
+						// -1 signifies that its still singleton
+	map <string, int> Atts;	// <attribute-name, distinct values>
+
+	AttsStats(): numTuples(0), numPartition(-1)
+	{}		
+};
 
 
 class Statistics
 {
+private:
+	int m_nPartitionNum;
+	map <string, AttsStats> m_mRelStats;
+	map <int, vector<string> > m_mPartitionInfoMap;
+ 	
 public:
 	Statistics();
 	Statistics(Statistics &copyMe);	 // Performs deep copy
@@ -21,6 +43,21 @@ public:
 	void  Apply(struct AndList *parseTree, char *relNames[], int numToJoin);
 	double Estimate(struct AndList *parseTree, char **relNames, int numToJoin);
 
+	// getter functions
+	int GetPartitionNumber()
+	{
+		return m_nPartitionNum;
+	}
+	
+	map<string, AttsStats> * GetRelStats()
+	{
+		return &m_mRelStats;	// return pointer to m_mRelStats
+	}
+	
+	map<int, vector<string> > * GetPartitionInfoMap()
+	{
+		return &m_mPartitionInfoMap; // return pointer to PartitionInfoMap
+	}
 };
 
 #endif
