1c1cc960f16570c03001031e83cea90ce145b34f
malvika85@gmail.com <malvika85@gmail.com@0cb6586d-dc45-c2ee-bce3-b71acda30c77>
2011-04-23 23:53:46 +0000

    print 2 combo

--- src/src/main.cc
+++ src/src/main.cc
@@ -12,7 +12,7 @@ extern "C" {
 extern struct FuncOperator *finalFunction;
 extern struct TableList *tables;
 
-void PrintFuncOperator(struct FuncOperator *finalFunction)
+/*void PrintFuncOperator(struct FuncOperator *finalFunction)
 {
         int i = 1;
         struct FuncOperator *temp;
@@ -20,6 +20,7 @@ void PrintFuncOperator(struct FuncOperator *finalFunction)
         while(temp != NULL)
         {
             cout << "Node = "<< i++ <<endl;
+			cout << "\t[42: *, 43: +, 45: -, 47: /\n";
             cout << "\t Code = " << temp->code << endl;
             cout << "\t FuncOperand:" << endl;
             if(temp->leftOperand != NULL)
@@ -35,6 +36,61 @@ void PrintFuncOperator(struct FuncOperator *finalFunction)
 
         if(finalFunction == NULL)
             cout << "finalFunction is null" <<endl;
+}*/
+
+void PrintFuncOperator(struct FuncOperator *finalFunction)
+{
+/* Structure of FuncOperator
+   -------------------------
+
+			NODE
+			- code [0: leaf node, 42: *, 43: +, 45: -, 47: /]
+			- leftOperand
+				- code [1: double, 2: int, 3: name, 4: string, 5: less_than, 6: greater_than, 7: equals]
+				- value
+			- leftOperator (left child)
+			- right (right child)
+
+			example: (a.b + b)
+	
+			code: +
+			leftOperand: NULL
+			leftOperator		right
+				/				  \
+			   /				   \
+		code: 0					code: 0
+		leftOperand				leftOperand
+			- name a.b				- name b
+		leftOperator: NULL		leftOperator: NULL
+		right: NULL				right: NULL			
+
+*/
+        static int pfo2_i = 1;
+		if (finalFunction == NULL)
+		{
+			//cout << "finalFunction is null" <<endl;
+			return;
+		}
+
+        struct FuncOperator *temp;
+        temp = finalFunction;
+
+        cout << "Node = "<< pfo2_i++ <<endl;
+		// if code = 0, indicates it is a leaf node, otherwise as follows:
+        cout << "\t[0: leaf node, 42: *, 43: +, 45: -, 47: / ]\n";
+        cout << "\t Code = " << temp->code << endl;
+        cout << "\t FuncOperand:" << endl;
+        if(temp->leftOperand != NULL)
+        {
+			cout << "\t\t[1: double, 2: int, 3: name, 4: string, 5: less_than, 6: greater_than, 7: equals]\n";
+            cout << "\t\t Code: " << temp->leftOperand->code << endl;
+            cout << "\t\t Value: " << temp->leftOperand->value << endl;
+        }
+        else
+            cout << "\t\tNULL" << endl;	// NULL indicates it has children
+
+		PrintFuncOperator(temp->leftOperator);	// print left subtree
+		PrintFuncOperator(temp->right);			// print right subtree
 }
 
 void PrintTableList(struct TableList *tables)
@@ -56,6 +112,46 @@ void PrintTableList(struct TableList *tables)
             cout << "tables specified is null" << endl;
 }
 
+void PrintTableCombinations(struct TableList *tables)
+{
+	/* Logic:
+		1. Put table names in a map --> will get sorted automagically
+		2. Take them out and put in a vector (easier to iterate over)
+		3. Make combinations
+	*/
+
+	// Step 1
+	struct TableList *p_TblList = tables;
+	map <string, int> table_map;
+	while (p_TblList != NULL)
+	{
+		table_map[p_TblList->tableName] = 1;
+		p_TblList = p_TblList->next;
+	}
+
+	// Step 2
+	vector <string> vTableName;
+	map <string, int>::iterator map_itr = table_map.begin();
+	for (; map_itr != table_map.end(); map_itr++)
+	{
+		vTableName.push_back(map_itr->first);
+	}
+
+	// Step 3
+	vector <string> vTable2Names;
+	cout << "\n\n--- Table combinations ---\n";
+	int len = vTableName.size();
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = i+1; j < len; j++)
+		{
+			cout << vTableName.at(i) << "." << vTableName.at(j) << endl;
+			vTable2Names.push_back(vTableName.at(i) + "." + vTableName.at(j));
+		}
+	}
+	cout << endl;
+}
+
 int main () {
     yyparse();
 
@@ -64,6 +160,8 @@ int main () {
 
     //print Tables list
     PrintTableList(tables);
+
+	PrintTableCombinations(tables);
 
     char *fileName = "Statistics.txt";
     Statistics::PrepareStatisticsFile(fileName);
